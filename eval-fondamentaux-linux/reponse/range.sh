f0b217c5f27abc47870961c442fc0e694c9d5d4a
thegesse <goose.minipainting@gmail.com>
2026-05-01 18:05:30 +0200

    add indempotence and collisions

--- eval-fondamentaux-linux/reponse/range.sh
+++ eval-fondamentaux-linux/reponse/range.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#i!/bin/bash
 
 
 folder=$@
@@ -12,11 +12,12 @@ fi
 mkdir -p range/{logs,csv,textes,images,divers}
 cp -vR ../desordre/* range/
 
-find range -type f | while read -r file; do
+find range -type f -print0 | while IFS= read -r -d '' file; do
+
 	#format file
 	new="$file"
 
-	#lowercase, remove (), turn space to _
+	#lowercase, remove (), turn space to _, avoid repeat ___ 
 	new="${new,,}"
 	new="${new//[()]/}"
 	new="${new// /_}"
@@ -29,14 +30,35 @@ find range -type f | while read -r file; do
 
 	#mv files
 	if [[ "$file" =~ \.log$ ]]; then
-		mv "$file" range/logs/		
+		dir="range/logs/"		
 	elif [[ "$file" =~ \.csv$ ]]; then
-		mv "$file" range/csv/
+		dir="range/csv/"
 	elif [[ "$file" =~ \.(txt|md)$ ]]; then
-		mv "$file" range/textes/
+		dir="range/textes/"
 	elif [[ "$file" =~ \.(png|jpg|jpeg)$ ]]; then
-		mv "$file" range/images/
+		dir="range/images/"
 	else
-		mv "$file" range/divers/
+		dir="range/divers/"
 	fi
+
+	#persistance n numbers or sumn
+	mkdir -p "$dir"
+	 base="$(basename "$file")"
+	 if [[ "$base" == *.* ]]; then
+		 name="${base%.*}"
+		 name="$(sed 's/_[0-9]\+$//' <<< "$name")"
+		 ext=".${base##*.}"
+	else
+		name="$base"
+		ext=""
+	fi
+
+	target="$dir/$name$ext"
+
+	i=1
+	while [[ -e "$target" ]]; do
+		target="$dir/${name}_$i$ext"
+		((i++))
+	done
+	mv "$file" "$target"
 done
